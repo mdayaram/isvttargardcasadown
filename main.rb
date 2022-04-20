b5aba1171c27e751ffaf7a532b607facf32e474e
Manoj Dayaram <m@noj.cc>
2022-04-19 17:22:07 -0700

    add source

--- main.rb
+++ main.rb
@@ -2,14 +2,18 @@ require "sinatra"
 require "http"
 
 get "/" do
+  body = ""
   begin
     response = HTTP.head("https://vtt.asgard.casa/")
     if response.code < 400
-      return "VTT Asgard Casa is UP"
+      body = "VTT Asgard Casa is UP"
     else
-      return "VTT Asgard Casa is DOWN"
+      body = "VTT Asgard Casa is DOWN"
     end
   rescue
-    return "VTT Asgard Casa is DOWN"
+    body = "VTT Asgard Casa is DOWN"
   end
+
+  body += "\n\n<br/><br/>Source: https://github.com/mdayaram/isvttasgardcasadown"
+  return body
 end
